dff586e430b6eddd7fba0c7a1e7ffe8200fe6a4f
Putu <nasi.be.guling@gmail.com>
2013-12-19 13:09:45 +0700

    initial git commit

---
+++ _the_cipher/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+using _encryption;
+
+
+namespace _the_cipher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text.Trim()))
+                MessageBox.Show(@"MASUKKAN PASSWORD!");
+            else
+                textBox1.Text = CStringCipher.Encrypt(textBox2.Text, textBox3.Text.Trim());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text.Trim()))
+                MessageBox.Show(@"MASUKKAN PASSWORD!");
+            else
+            {
+                try
+                {
+                    textBox1.Text = CStringCipher.Decrypt(textBox2.Text, textBox3.Text.Trim());
+                }
+                catch
+                {
+                    MessageBox.Show(@"PASSWORD ANDA SALAH ATAU DATA TIDAK DITEMUKAN!");
+                }
+            }
+        }
+    }
+}
